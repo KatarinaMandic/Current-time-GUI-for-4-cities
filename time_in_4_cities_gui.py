f74e0ee0e6b7678d5fc8d6de6a6a11dd18a985e9
KatarinaMandic <katarina.mandic.97@gmail.com>
2022-11-05 19:52:45 +0100

    Modified time_in_4_cities.py

--- time_in_4_cities_gui.py
+++ time_in_4_cities_gui.py
@@ -8,7 +8,8 @@ layout = [
     sg.Button('Dublin', size = (10,1)),
     sg.Button('Belgrade', size = (10,1)),
     sg.Button('Tokyo', size = (10,1))],
-    [sg.Text('', size = (40,1))]
+    [sg.Text('')],
+    [sg.Text('', key = '_TIME_', size = (40,1), justification='c')]
 ]
 
 window = sg.Window(title = 'Current time', layout = layout, font = ('Arial', 16))
@@ -18,12 +19,16 @@ while True:
     if event == None:
         break
     if event == 'New York':
-        pass
+        time = arrow.now('America/New_York').ctime()
+        window['_TIME_'].Update(f"{time}")
     if event == 'Dublin':
-        pass
+        time = arrow.now('Europe/Dublin').ctime()
+        window['_TIME_'].Update(f"{time}")
     if event == 'Belgrade':
-        pass
+        time = arrow.now('Europe/Belgrade').ctime()
+        window['_TIME_'].Update(f"{time}")
     if event == 'Tokyo':
-        pass
+        time = arrow.now('Asia/Tokyo').ctime()
+        window['_TIME_'].Update(f"{time}")
 
 window.close()
